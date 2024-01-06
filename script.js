05440a9e7eec9a1b63e813b0bc563f4d6013fb0d
yuto-0315 <94541673+yuto-0315@users.noreply.github.com>
2024-01-06 14:55:05 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,55 @@
 // CSVファイルを読み込む関数
-function readCSV() {
-    // XMLHttpRequestを使用してCSVファイルを読み込む
+function readCSV(csvPath, tableId) {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
-                displayCSV(xhr.responseText);
+                displayCSV(xhr.responseText, tableId);
             } else {
                 console.error('Failed to load CSV');
             }
         }
     };
-    xhr.open('GET', 'data.csv', true); // data.csvには実際のCSVファイルのパスを指定
+    xhr.open('GET', csvPath, true);
     xhr.send();
 }
 
 // 読み込んだCSVデータを表形式で表示する関数
-function displayCSV(csvData) {
-    var table = document.getElementById('csvTable');
+function displayCSV(csvData, tableId) {
+    var table = document.getElementById(tableId);
     var rows = csvData.split('\n');
 
     for (var i = 0; i < rows.length; i++) {
         var cells = rows[i].split(',');
-        var row = table.insertRow(-1); // 新しい行を追加
+        var row = table.insertRow(-1);
 
         for (var j = 0; j < cells.length; j++) {
             var cell;
-            if (i === 0) { // 1行目の場合
-                cell = document.createElement('th'); // th要素を作成
+            if (i === 0) {
+                cell = document.createElement('th');
             } else {
-                cell = document.createElement('td'); // 通常のセルはtd要素を作成
+                cell = document.createElement('td');
             }
 
-            if (j === 1 && i > 0) { // 2列目でかつ1行目以外の場合
-                var link = document.createElement('a'); // a要素を作成
-                link.href = cells[j]; // リンク先をセット
-                link.textContent = cells[j - 1]; // テキストをセット
-                cell.appendChild(link); // セルにリンクを追加
+            if (j === 1 && i > 0) {
+                var link = document.createElement('a');
+                link.href = cells[j];
+                link.textContent = cells[j - 1];
+                cell.appendChild(link);
             } else {
-                cell.textContent = cells[j]; // 通常のセルにCSVデータを挿入
+                cell.textContent = cells[j];
             }
 
-            row.appendChild(cell); // 行にセルを追加
+            row.appendChild(cell);
         }
     }
 }
 
 // ページが読み込まれた時にCSVを読み込む
 document.addEventListener('DOMContentLoaded', function() {
-    readCSV();
+
+    readCSV("total.csv", "total_table");
+    readCSV("office.csv", "office_table");
+    readCSV("music.csv", "music_table");
+
 });
